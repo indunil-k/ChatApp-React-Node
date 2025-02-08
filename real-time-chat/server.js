12506ca5e93ea6b2c0b3fb5c398ba2280767a2c5
indunil-k <wakindunil@gmail.com>
2025-02-09 00:07:48 +0530

    add modern styles

--- real-time-chat/server.js
+++ real-time-chat/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Set up CORS configuration
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Replace with your client URL
+    origin: "http://localhost:5174", // Replace with your client URL
     methods: ["GET", "POST"],
   }
 });
